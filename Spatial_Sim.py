edf981638846b6740e7a81a6c6fd2fcd9d4a41dc
barbosaleo98 <64433982+barbosaleo98@users.noreply.github.com>
2022-03-06 23:37:27 -0300

    Adicionado Spatial_Sim atualizado
    Adicionado Spatial_Sim.py estruturado de maneira mais organizada, com nomes de variáveis mais intuitivos e comentários mais concisos.

--- Spatial_Sim.py
+++ Spatial_Sim.py
@@ -1,121 +1,118 @@
-# Realiza a simulação geométrica estocástica para representação espacial dos eleemntos
-# Baseado em um script  do Autor: H. Paul Keeler, 2019.
+# Realiza a simulação geométrica estocástica para representação espacial de uma rede V2X
+
+# Busca reproduzir resultados de Wang et al, 2020
+# https://doi.org/10.48550/arXiv.2009.14500
+
+# Baseado em scripts de H. Paul Keeler, 2019.
 # Repositório: github.com/hpaulkeeler/posts
-# Post utilizado como base: hpaulkeeler.com/simulating-a-cox-point-process-based-on-a-poisson-line-process/
 
-import numpy as np;  # Pacote NumPy para vetores, gerador de números aleatórios, etc
-import matplotlib.pyplot as plt  # para plotagem
-from matplotlib import collections  as mc  # para plotar segmentos de linhas
+import numpy as np;  
+import matplotlib.pyplot as plt  
+from matplotlib import collections  as mc 
 
-plt.close('all');  # fecha as figuras já abertas
+plt.close('all')
 
-###START -- Parâmetros -- START###
-# Parâmetros do processo de Cox (PLP) 
-# O processo de pontos de Cox é realizado primeiramente simulando um PLP
-# Em cada linha (ou segmento) um PLP independente é então simulado
+### ÁREA CIRCULAR ###
 
-lambda0 = 1e-3/np.pi;  # intensidade (densidade média) do PLP
-mu = 1e-3; # intensidade (densidade média) do PPP 
+# Parâmetros: origem em (x0,y0) e raio r
+x0 = 0;  
+y0 = 0;  
+r = 3000;  
 
-# Dimensões do disco
-xx0 = 0;  # Centro x do disco
-yy0 = 0;  # Centro y do disco
-r = 3000;  # Raio do disco
-massLine = 2 * np.pi * r * lambda0;
-areaTotal=np.pi*r**2; # Área do disco
-###END -- Parâmetros -- END###
-
-###START -- Simula um PLP no disco -- START###
-# Simula o PPP
-numbLines = np.random.poisson(massLine);  # Número de pontos de Poisson
-
-theta = 2 * np.pi * np.random.rand(numbLines);  # seleciona componente angular uniformemente
-p = r * np.random.rand(numbLines);  # seleciona componente radial uniformemente
-q = np.sqrt(r ** 2 - p ** 2);  # distância da borda do círculo (alonge line)
-
-# calcula componentes trigonométricas
-sin_theta = np.sin(theta);
-cos_theta = np.cos(theta);
-
-# calcula os pontos finais dos segmentos do PPP
-xx1 = xx0 + p * cos_theta + q * sin_theta;
-yy1 = yy0 + p * sin_theta - q * cos_theta;
-xx2 = xx0 + p * cos_theta - q * sin_theta;
-yy2 = yy0 + p * sin_theta + q * cos_theta;
-###END Simula um PLP no disco END###
-
-def PPP_roads(mu,p,q,sin_theta,cos_theta,xx0,yy0):
-    ###START Simula um PPP em cada linha START###
-    lengthLine = 2 * q;  # length of each segment
-    massPoint = mu * lengthLine;  # mass on each line
-    numbLinePoints = np.random.poisson(massPoint);  # Poisson number of points on each line
-    numbLinePointsTotal = sum(numbLinePoints);
-    uu = 2 * np.random.rand(numbLinePointsTotal) - 1;  # uniform variables on (-1,1)
-
-    # replica os valores para simular todos pontos em um único passo
-    xx0_all = np.repeat(xx0, numbLinePointsTotal);
-    yy0_all = np.repeat(yy0, numbLinePointsTotal);
-    p_all = np.repeat(p, numbLinePoints);
-    q_all = np.repeat(q, numbLinePoints);
-    sin_theta_all = np.repeat(sin_theta, numbLinePoints);
-    cos_theta_all = np.repeat(cos_theta, numbLinePoints);
-
-    # Posiciona os pontos nos segmentos lineares de Poisson
-    xxPP_all = xx0_all + p_all * cos_theta_all + q_all * uu * sin_theta_all;
-    yyPP_all = yy0_all + p_all * sin_theta_all - q_all * uu * cos_theta_all;
-    ### FIM Simula um PPP em cada linha  ###
-    return xxPP_all, yyPP_all
-
-
-xxPP_transmitter, yyPP_transmitter = PPP_roads(mu,p,q,sin_theta,cos_theta,xx0,yy0)
-xxPP_eves, yyPP_eves = PPP_roads(1e-3,p,q,sin_theta,cos_theta,xx0,yy0)
-
-### START Plotaegem do PLP ###
-# plota o círculo
+# Plota o círculo com um 'x' na origem
 t = np.linspace(0, 2 * np.pi, 200);
 xp = r * np.cos(t);
 yp = r * np.sin(t);
 fig, ax = plt.subplots();
-ax.plot(xx0 + xp, yy0 + yp, 'r--');
+ax.plot(x0 + xp, y0 + yp, 'r--');
+plt.scatter(x0,y0, marker='x',color='r')
 plt.axis('equal');
 
-# plota segmentos de PLP
-# Cria uma lista para plotar os segmentos
-segments = [[(xx1[i], yy1[i]), (xx2[i], yy2[i])] for i in range(numbLines)];
-lc = mc.LineCollection(segments, colors='b')
-ax.add_collection(lc)  # plota os segmentos criados
 
-# plota pontos de Poisson nas linhas
-plt.plot(xxPP_transmitter, yyPP_transmitter, 'g^');
-plt.plot(xxPP_eves, yyPP_eves, 'ko');
+### PROCESSO PONTUAL DE POISSON (PPP) - NÓS PLANARES ###
 
-###END Plotagem do PLP END###
+## Função que produz os nós planares através de um PPP
+def planarNodes(lambda_PPP, radius):
+    area_circle = np.pi*radius**2
+    num_Points_PPP = np.random.poisson(lambda_PPP*area_circle)
+    theta_PPP = 2*np.pi*np.random.uniform(0,1,num_Points_PPP)
+    rho_PPP = radius*np.sqrt(np.random.uniform(0,1,num_Points_PPP))
+    x_planar = rho_PPP * np.cos(theta_PPP)
+    y_planar = rho_PPP * np.sin(theta_PPP)
+    return x_planar,y_planar, num_Points_PPP
 
-## Criação de função que cria os pontos de um PPP
-def PPP_points(lambda_process, area_circle, radius):
-    num_Points = np.random.poisson(lambda_process*area_circle)
-    theta = 2*np.pi*np.random.uniform(0,1,num_Points)
-    rho = radius*np.sqrt(np.random.uniform(0,1,num_Points))
-    xx = rho * np.cos(theta)
-    yy = rho * np.sin(theta)
-    return xx,yy
-
-# Parâmetros do PPP para Alices
-lambda_transmitter=1e-6; # Intensidade (densidade média) do PPP
+# Geração dos nós planares 
+lambda_alices=1*1e-6; 
+lambda_eves=1*1e-6; 
+x_planar_alices, y_planar_alices , num_planar_alices = planarNodes(lambda_alices, r)
+x_planar_eves, y_planar_eves, num_eves = planarNodes(lambda_eves, r)
  
-# Simula PPP para Alices
-xx_transmitter, yy_transmitter = PPP_points(lambda_transmitter, areaTotal, r)
- 
-#Plotta os Alices
-plt.scatter(xx_transmitter,yy_transmitter, marker='s', color='cyan' );
+#Plotta os nós planares
+plt.scatter(x_planar_alices, y_planar_alices, marker='s', color='cyan' );
+plt.scatter(x_planar_eves, y_planar_eves, marker='*', color='mediumorchid')
 
-# Parâmetros do PPP para Eves
-lambda_eves=1e-6; # Intensidade (densidade média) do PPP
- 
-# Simula PPP para Eves
-xx_eves, yy_eves = PPP_points(lambda_eves, areaTotal, r)
- 
-#Plota os Eves
-plt.scatter(xx_eves,yy_eves, marker='*', color='purple')
-plt.title('Simulação Espacial baseada em (WANG, C. et al., 2020)')
+### PROCESSO LINEAR DE POISSON (PLP) - RUAS DA REDE ###
+
+lambda_roads = 1e-3/np.pi;  # intensidade do PLP
+
+# Simula um PLP na área circular
+massLine = 2 * np.pi * r * lambda_roads; 
+numLines = np.random.poisson(massLine);  
+theta_PLP = 2 * np.pi * np.random.rand(numLines);  # seleciona componente angular uniformemente
+p = r * np.random.rand(numLines);  # seleciona componente radial uniformemente
+q = np.sqrt(r ** 2 - p ** 2);  # distância do midpoint para a borda do círculo 
+sin_theta_PLP = np.sin(theta_PLP);
+cos_theta_PLP = np.cos(theta_PLP);
+
+# calcula os pontos finais dos segmentos do PLP
+x1 = x0 + p * cos_theta_PLP + q * sin_theta_PLP;
+y1 = y0 + p * sin_theta_PLP - q * cos_theta_PLP;
+x2 = x0 + p * cos_theta_PLP - q * sin_theta_PLP;
+y2 = y0 + p * sin_theta_PLP + q * cos_theta_PLP;
+
+# plota segmentos do PLP
+segments = [[(x1[i], y1[i]), (x2[i], y2[i])] for i in range(numLines)];
+lc = mc.LineCollection(segments, colors='b', cmap='jet', alpha=0.7)
+ax.add_collection(lc)  
+
+## Pontos para definir e plotar a rua lo (opcional, descomentar para habilitar)
+#xo_1 = -r
+#xo_2 = r 
+#plt.plot((xo_1, xo_2), (y0,y0), color='darkorchid', linewidth=1.5)
+
+
+### PROCESSO LINEAR COX DE POISSON (PLCP) - NÓS VEICULARES ###
+
+# Função que gera os nós veiculares através de um PLCP
+def vehicularNodes(mu,p,q,sin_theta,cos_theta,x0,y0):
+    lengthLine = 2 * q;  
+    massPoint = mu * lengthLine;  # intensidade de cada PPP
+    num_Points_PLCP = np.random.poisson(massPoint);  
+    Total_Points_PLCP = sum(num_Points_PLCP);
+    uu = 2 * np.random.rand(Total_Points_PLCP) - 1;  
+
+    # replica os valores para simular todos pontos em um único passo
+    x0_all = np.repeat(x0, Total_Points_PLCP);
+    y0_all = np.repeat(y0, Total_Points_PLCP);
+    p_all = np.repeat(p, num_Points_PLCP);
+    q_all = np.repeat(q, num_Points_PLCP);
+    sin_theta_all = np.repeat(sin_theta, num_Points_PLCP);
+    cos_theta_all = np.repeat(cos_theta, num_Points_PLCP);
+
+    # Posiciona os pontos nos segmentos lineares 
+    x_vehicular_all = x0_all + p_all * cos_theta_all + q_all * uu * sin_theta_all;
+    y_vehicular_all = y0_all + p_all * sin_theta_all - q_all * uu * cos_theta_all;
+
+    return x_vehicular_all, y_vehicular_all
+
+# Gera os nós veiculares Alices e Eves
+mu_alices = 1e-3;
+mu_eves = 1e-3; 
+x_vehicular_alices, y_vehicular_alices = vehicularNodes(mu_alices,p,q,sin_theta_PLP,cos_theta_PLP,x0,y0)
+x_vehicular_eves, y_vehicular_eves = vehicularNodes(mu_eves,p,q,sin_theta_PLP,cos_theta_PLP,x0,y0)
+
+# plota os nós veiculares
+plt.scatter(x_vehicular_alices, y_vehicular_alices, color = 'lime', marker = '^');
+plt.scatter(x_vehicular_eves, y_vehicular_eves, color='black', marker='o');
+
 plt.show()
